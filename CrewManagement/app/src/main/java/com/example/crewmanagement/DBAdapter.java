1f6a4973518a8df106e26633c861b693dc9b4939
Stephen Perrin <Sperrin7009@conestogac.on.ca>
2020-03-11 14:09:07 -0400

    1. Added InsertNewMember method to DBAdapter class (Needs testing)

--- CrewManagement/app/src/main/java/com/example/crewmanagement/DBAdapter.java
+++ CrewManagement/app/src/main/java/com/example/crewmanagement/DBAdapter.java
@@ -4,9 +4,13 @@ import com.example.crewmanagement.Data;
 
 import java.io.File;
 import java.lang.String;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -15,6 +19,16 @@ import android.util.Log;
 
 public class DBAdapter
 {
+    // RetCode Meanings
+    public static final int SUCCESS = 1;
+    public static final int FAILURE = 0;
+    public static final int INVALID_USERNAME = -1;
+    public static final int INVALID_PASSWORD = -2;
+    public static final int INVALID_NAME = -3;
+    public static final int AGE_NOT_IN_RANGE = -4;
+    public static final int INVALID_DATE_FORMAT = -5;
+    public static final int INVALID_JOB_TITLE = -6;
+
     // Database Constants
     private static final String DB_NAME = " CrewManagementDBVersion1";
     private static final int DB_VERSION = 1;
@@ -162,14 +176,52 @@ public class DBAdapter
         }
     }
 
-    // Public Methods
-    public Data GetData()
+    /*
+     * FUNCTION:
+     *		getNumberOfMembers()
+     * DESCRIPTION:
+     *		Queries the database to find the number of members (including admin)
+     * PARAMETERS:
+     *			N/A
+     * RETURNS:
+     *			int : Returns the number of members in the database (this value can double as the new MemberID)
+     */
+    private int getNumberOfMembers()
     {
         // Variables
         Cursor cursor;
+        int count = 0;
+
+        this.openReadableDB();
+
+        cursor = db.rawQuery("SELECT MemberID FROM Members", null);
+
+        while(cursor.moveToNext())
+        {
+            count++;
+        }
+
+        return count;
+    }
+
+    // Public Methods
+    /*
+     * FUNCTION:
+     *		getData()
+     * DESCRIPTION:
+     *		Queries the database to get all information related to the variables in the Data class to be filled and returned
+     * PARAMETERS:
+     *			N/A
+     * RETURNS:
+     *			Data : Returns a populated Data class object with ALL database data
+     */
+    public Data GetData()
+    {
+        // Variables
+        int counter = 0;
+        Cursor cursor;
         Data newData = new Data();
         ArrayList<String> memberIDs = new ArrayList<String>();
-        int counter = 0;
         ArrayList<String> username = new ArrayList<String>();
         ArrayList<String> password = new ArrayList<String>();
         ArrayList<String> eName = new ArrayList<String>();
@@ -278,7 +330,173 @@ public class DBAdapter
         newData.jobList = jobList;
         newData.jobStatus = jobStatus;
 
+        this.closeCursor(cursor);
+        this.closeDB();
+
         return newData;
+    }
+
+    /*
+     * FUNCTION:
+     *		InsertNewMember(String user, String pass, String name, Integer age, String doh, Integer access, String phone, byte[] profilePic)
+     * DESCRIPTION:
+     *		Takes Data from the parameters and inserts them into the User and Members Tables.
+     * PARAMETERS:
+     *			String user       : The new member's username (Must not contain a space, ',', or ''')
+     *          String pass       : The new member's password (Must not contain a space)
+     *          String name       : The new member's name (Is parsed into a first and last name; first and last name must not have additional spaces; last name can be blank)
+     *          Integer age       : The new member's age (Age must be between 15 and 80)
+     *          String doh        : The new member's DateOfHire (******NOTE: DateOfHire can be set to null in parameters to enter default value (Current Date in YYYY-MM-DD format) into database*****)
+     *          Integer access    : The new member's access (*****NOTE: access can be set to null in parameters to enter default value (0) into database*****)
+     *          String phone      : The new member's phone (*****NOTE: phone can be set to null in parameters to enter default value ("000-000-0000") into database*****)
+     *          byte[] profilePic : The new member's profile picture (*****NOTE: ProfilePicture can be set to null in parameters to enter default value (null) into database*****)
+     * RETURNS:
+     *			long : Returns the RowID of the newly inserted row on success, else returns one of the class designated retValues
+     */
+    public long InsertNewMember(String user, String pass, String name, Integer age, String doh, Integer access, String phone, byte[] profilePic)
+    {
+        // Variables
+        long retValue = SUCCESS;
+        int memberID = getNumberOfMembers();
+        ContentValues cv = new ContentValues();
+        String fName = "";
+        String lName = "";
+
+        // Open writeable database
+        this.openWriteableDB();
+
+        // Check that the name is valid (for security of database)
+        if (name.contains("1") || name.contains("2") || name.contains("3") || name.contains("4") || name.contains("5") || name.contains("6") || name.contains("7") || name.contains("8") || name.contains("9") || name.contains("0"))
+        {
+            return INVALID_NAME;
+        }
+        else if (name.contains(" "))
+        {
+            fName = name.substring(0, name.indexOf(' '));
+            lName = name.substring(name.indexOf(' '), name.length());
+
+            if (fName.contains(" ") || lName.contains(" "))
+            {
+                return INVALID_NAME;
+            }
+            else
+            {
+                cv.put("FirstName", fName);
+                cv.put("LastName", lName);
+            }
+        }
+        else
+        {
+            fName = name;
+
+            cv.put("FirstName", fName);
+            cv.put("LastName", lName);
+        }
+
+        // Check that the age is within acceptable range
+        if (age <= 15 || age >= 80)
+        {
+            return AGE_NOT_IN_RANGE;
+        }
+        else
+        {
+            cv.put("Age", age);
+        }
+
+        // Check that the date of hire is in the correct format else if null/"Unknown" then set to today's date (in format YYYY-MM-DD)
+        if (!doh.matches("^((19|2[0-9])[0-9]{2})-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$"))
+        {
+            return INVALID_DATE_FORMAT;
+        }
+        else if (doh == "Unknown" || doh == null)
+        {
+            Date currDate = Calendar.getInstance().getTime();
+            SimpleDateFormat df = new SimpleDateFormat("YYYY-MM-DD");
+
+            doh = df.format(currDate);
+        }
+
+        // Store the DateOfHire
+        cv.put("DateOfHire", doh);
+
+        // Check if phone is null then set to default format with Zeros
+        if (phone == null)
+        {
+            phone = "000-000-0000";
+        }
+        else
+        {
+            cv.put("Phone", phone);
+        }
+
+        // Store profile pic
+        cv.put("ProfilePicture", profilePic);
+
+        // Store the memberID
+        cv.put("MemberID", memberID);
+
+        // Insert into members table
+        retValue = db.insert("Members", null, cv);
+
+        // If an error occurred during insert return failure code
+        if (retValue == -1)
+        {
+            return FAILURE;
+        }
+
+
+        // Reset the contentValue variable for new data
+        cv = new ContentValues();
+        ///////////////////////////////////////////////
+
+        // Check that the username is valid (for security of database)
+        if (user.contains(" ") || user.contains("'") || user.contains(","))
+        {
+            return INVALID_USERNAME;
+        }
+        else
+        {
+            cv.put("Username", user);
+        }
+
+        // Check that the password is valid (for security of database)
+        if (pass.contains(" ") || pass.contains("'") || pass.contains(","))
+        {
+            return INVALID_PASSWORD;
+        }
+        else
+        {
+            cv.put("Password", pass);
+        }
+
+        // Check that the job title is valid else if null set to default employee value (0)
+        if (!(access >= 0 && access <= 1))
+        {
+            return INVALID_JOB_TITLE;
+        }
+        else if (access == null)
+        {
+            access = 0;
+        }
+        else
+        {
+            cv.put("Access", access);
+        }
+
+        // Put the memberID foreign key
+        cv.put("MemberID", memberID);
+
+        // Insert into the users table
+        retValue = db.insert("Users", null, cv);
+
+        // If an error occurred during insert return failure code
+        if (retValue == -1)
+        {
+            return FAILURE;
+        }
+
+        // Return the RowID of the newly inserted row
+        return retValue;
     }
 
 }
