e7782674bfaf26f8c3ef877b5752b2d3578e5ae8
Marissa Schmitt <Mschmitt0077@conestogac.on.ca>
2020-03-12 23:33:27 -0400

    Menu/layout for View screen (Members + progress screen)

--- CrewManagement/app/src/main/java/com/example/crewmanagement/ViewScreen.java
+++ CrewManagement/app/src/main/java/com/example/crewmanagement/ViewScreen.java
@@ -9,11 +9,18 @@
  */
 package com.example.crewmanagement;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ViewScreen extends AppCompatActivity {
+
+    Data info;// data class to hold the information needed for the app
+
     @Override
     /*
      * FUNCTION:     onCreate
@@ -28,6 +35,88 @@ public class ViewScreen extends AppCompatActivity {
         setContentView(R.layout.view_screen);
 
         setTitle("View Screen");
+    }
+
+
+    /*
+     * FUNCTION:	 onCreateOptionsMenu
+     * DESCRIPTION:	This function is event handler for whenever the options menu is created
+     * PARAMETERS:	Menu menu - this parameter indicates what menu is created
+     * RETURNS:	    bool - this function returns a boolean value based on it's operation
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+
+        Intent intent = getIntent();//creating a new intent
+        Bundle data = intent.getExtras();//getting extras from the intent
+        info = (Data)data.getSerializable("data");
+        //inflating the specified menu for the administrator screen
+        getMenuInflater().inflate(R.menu.view_screen, menu);
+        return true;
+    }
+
+
+
+    /*
+     * FUNCTION:	 onOptionsItemSelected
+     * DESCRIPTION:	This function is event handler for whenever an option within the menu has been selected
+     * PARAMETERS:	MenuItem item - this parameter indicates what item has been selected
+     * RETURNS:	    bool - this function returns a boolean value based on it's operation
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch(item.getItemId())
+        {
+            case R.id.menu_activity1:
+                Intent intent1 = new Intent(ViewScreen.this, Administrator.class);
+                //putting the Data object into the extras
+                intent1.putExtra("data", info);
+                //starting the new activity
+                startActivity(intent1);
+                Log.i("View","Going to adminstrator screen.");
+                finish();
+                return true;
+            case R.id.menu_activity2:
+                Intent intent2 = new Intent(ViewScreen.this, MembersScreen.class);
+                //putting the Data object into the extras
+                intent2.putExtra("data", info);
+                //starting the new activity
+                startActivity(intent2);
+                Log.i("View","Going to members screen.");
+                finish();
+                return true;
+            case R.id.menu_activity4:
+                //creating a new intent for the job assignment screen
+                Intent intent3 = new Intent(ViewScreen.this, JobAssignment.class);
+                //putting the Data object into the extras
+                intent3.putExtra("data", info);
+                //starting the new activity
+                startActivity(intent3);
+                Log.i("View","Going to the job assignment screen.");
+                finish();
+                return true;
+            case R.id.menu_activity5:
+                Intent intent4 = new Intent(ViewScreen.this, NewsFeed.class);
+                intent4.putExtra("data", info);
+                startActivity(intent4);
+                Log.i("View","Going to news feed screen.");
+                finish();
+                return true;
+            case R.id.menu_activity6:
+                //creating a new intent for the login screen
+                Intent intent5 = new Intent (ViewScreen.this, MainActivity.class);
+                //putting the Data object into the extras
+                intent5.putExtra("data", info);
+                //starting the new activity
+                startActivity(intent5);
+                Log.i("View","Going to login screen.");
+                finish();
+                return true;
+            default:
+                return false;
+        }
     }
 
 }
